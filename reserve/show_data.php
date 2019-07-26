9577772e2ce2aab8fff8d7994831ada257ad6e5b
ぅぅみかん <kokoko03040304030403@gmail.com>
2019-07-26 16:50:58 +0900

    reserve.phpにselect文で出した年月のデータの日にちと、開始時間をリストに入れる、そしてjsonにしてreserve.phpに渡す.

--- reserve/show_data.php
+++ reserve/show_data.php
@@ -1,26 +1,56 @@
 <?php
-    require_once('pdo.php');
 
     try {
-        $dbh = new PDO('mysql:host=localhost;dbname=Customers;charset=utf8mb4;','root','akogi0304');
-        $sql = "SELECT * FROM customers WHERE date_format(reserve_date,'%Y%m') = '201907'";  //2019年の7月を抽出.
+
+        $year = $_POST["yeara"];
+        $month=$_POST["month"];
+
+
+	    $reserve_ym = (string)$ydate.(string)$mdate; //reserve.phpで宣言された月.
+        //特定の年月のデータを抽出.
+        $dbh = new PDO('mysql:host=127.0.0.1;dbname=Customers;charset=utf8;','root','akogi0304');
+        $sql = "SELECT * FROM customers WHERE date_format(reserve_date,'%Y%m') = '201907'";
         $stmt = $dbh->prepare($sql);
         $stmt->execute();
-        $list[] = $stmt->fetchAll();
+        $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        if(empty($list[0])){
-            $list[0] = '予定なし';
-            //echo '6月1日に予定が入っていません.';
-        } else {
-            $list[0] = '予定あり';
-            //echo '6月1日に予定は入っています';
-        }
+        $stack = array(); //日にちを入れる箱.
+        $stack_start = array(); //開始時間を入れる箱.
+
+        foreach ($list as $row) {
+            for($i = 1; $i < 16; $i++){
+                if($row['reserve_date'] == '2019-07-0'.$i){ //20190701~20190709
+
+                    for($k = 9; $k < 24; $k++){
+                        if($row['start_time'] == $k.':00:00'){ //09:00:00
+                               array_push($stack,$i);
+                               array_push($stack_start,$k);
+                        }
+                    }
+                            //iが入っている日にちなので、それぞれの変数に格納する.
+                }else if($row['reserve_date'] == '2019-07-'.$i){//20190710~20190715
+                    for($k = 9; $k < 24; $k++){
+                        if($row['start_time'] == $k.':00:00'){ //09:00:00
+                               array_push($stack,$i);
+                               array_push($stack_start,$k);
+                        }
+                    }
+                }
+            }
+	    }
+
+        $stack_from_php = json_encode($stack);
+        // var_dump($stack_from_php);
+
+        $stack_start_from_php = json_encode($stack_start);
+        // var_dump($stack_start_from_php);
 
 
-        } catch (PDOException $e) {
+	} catch (PDOException $e) {
         echo "接続失敗: " . $e->getMessage() . "\n";
         exit();
     }
-
+    require_once('pdo.php');
+    require_once('reserve.php');
 
 ?>
